73b12187f15e97ab09a3a80bfa243c53f0ab41db
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-10 15:48:34 +0300

    creating app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import  Flask
+# from flask_bootstrap import Bootstrap
+from config import config_options
 
-# initializing application
-app=Flask(__name__)
+# bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
