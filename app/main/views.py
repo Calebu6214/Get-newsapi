9621ce04e5382c3ded3a2ecda86ad17385972089
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-10 23:33:37 +0300

    adding news details url

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..request import get_new,get_news
+from ..request import get_article, get_new,get_news
 
 # Views
 @main.route('/')
@@ -9,22 +9,21 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    all_news=get_news()
     title='Home-See your news'
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title,all_news=all_news)
 
 
-@main.route('/')
-def index():
+@main.route('/news/<int:id>')
+def news(id):
 
     '''
     View root page function that returns the index page and its data
     '''
 
     # Getting popular news
-    popular_news = get_news('popular')
-    print(popular_news)
-    popular_article=get_news('popular_article')
-    title = 'Home - News site'
+  
+    results=get_article(id)
 
     # search_news=request.args.get('news_query')
 
@@ -32,4 +31,4 @@ def index():
     #     return redirect(url_for('main.search',news_name=search_news))
     # else:
     #     return render_template('index.html', title = title,popular=popular_news)
-    return render_template('index.html', title = title,popular = popular_news,popular_article=popular_article)
+    return render_template('news.html', results = results,)
