f7e2aa40e031b1cc3e9366d6388954a298b938db
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-10 16:48:44 +0300

    wrting code to make request to our api

---
+++ app/request.py
@@ -0,0 +1,7 @@
+from .models import News
+
+#Getting api key
+apikey=None
+
+#getting the movie base url
+base_url=None
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=Apple&from=2021-09-10&sortBy={}&apiKey={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/news/{}& ?from={}& apikey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
